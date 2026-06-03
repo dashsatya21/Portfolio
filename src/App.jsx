677455ec0153eb4f96c8fdc479cbfe1ad9887aeb
innocenThug <satyabrata211297@gmail.com>
2026-06-03 14:22:56 +0530

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -776,7 +776,7 @@ function NavLogo() {
       height="38"
       style={{
         flexShrink: 0,
-        filter: `drop-shadow(0 0 15px ${color}99)`,
+        filter: `drop-shadow(0 0 25px ${color})`,
         transition: "filter 0.5s ease-in-out infinite",
       }}
     />
